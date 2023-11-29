356f15a86e2fd843eee6acdf9757db3115612b3d
Hugh Brown <hughdbrown@yahoo.com>
2023-11-28 17:30:23 -0700

    Add command line argument for config file

--- src/mail_org.py
+++ src/mail_org.py
@@ -1,11 +1,21 @@
 from pathlib import Path
 
+import click
+
 from .map_engine import MapEngine
 
-
-def main():
+@click.command()
+@click.option(
+    "--config-file",
+    "-c",
+    required=False,
+    type=str,
+    default="mail-org.json",
+)
+def main(config_file):
+    """Driver routine for using mail-org."""
     with MapEngine() as me:
-        me.apply(Path("mail-org.json"))
+        me.apply(Path(config_file))
 
 
 if __name__ == '__main__':
